ff83356f8a00c53e4b640d0fe49aaad904d60c6c
Deluxxexxx <alejandritoout@gmail.com>
2024-09-23 10:51:08 -0500

    commit inicial

---
+++ reservas/src/main/java/com/reservacomunitaria/app/ReservasApplication.java
@@ -0,0 +1,12 @@
+package com.reservacomunitaria.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasApplication.class, args);
+	}
+}
